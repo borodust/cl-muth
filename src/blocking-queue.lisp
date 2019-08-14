82c420ed4c4f593416f1ab603cfdf11554659fe7
Pavel Korolev <dev@borodust.org>
2019-08-14 09:03:01 +0300

    Use array instead of circular list for fifo

--- src/blocking-queue.lisp
+++ src/blocking-queue.lisp
@@ -2,6 +2,7 @@
 
 
 (define-constant +iterations-until-wait+ 1024)
+(define-constant +minimal-queue-extension+ 10)
 
 
 (deftype blocking-queue-item-priority ()
@@ -12,31 +13,68 @@
 ;;;
 (defstruct (fifo
             (:constructor make-fifo ()))
-  tail-tip)
+  (queue (make-array +minimal-queue-extension+ :initial-element nil))
+  (head-idx -1 :type fixnum)
+  (tail-idx -1 :type fixnum))
+
+
+(defun prev-idx (fifo idx)
+  (if (> idx 0)
+      (1- idx)
+      (length (fifo-queue fifo))))
+
+
+(defun next-idx (fifo idx)
+  (if (< idx (1- (length (fifo-queue fifo))))
+      (1+ idx)
+      0))
+
+
+(defun %fifo-extend (fifo)
+  (let* ((old-queue (fifo-queue fifo))
+         (new-queue (make-array (+ (length old-queue) +minimal-queue-extension+)
+                                :initial-element nil))
+         (head-idx (fifo-head-idx fifo))
+         (tail-idx (fifo-tail-idx fifo)))
+    (if (= head-idx 0)
+        (setf (subseq new-queue 0) old-queue)
+        (let* ((subhead-len (- (length old-queue) head-idx))
+               (head-view (make-array subhead-len :displaced-to old-queue
+                                                  :displaced-index-offset head-idx))
+               (tail-view (make-array (1+ tail-idx) :displaced-to old-queue)))
+          (setf (subseq new-queue 0) head-view
+                (subseq new-queue subhead-len) tail-view)))
+    (setf (fifo-queue fifo) new-queue
+          (fifo-head-idx fifo) 0
+          (fifo-tail-idx fifo) (1- (length old-queue)))))
 
 
 (defun fifo-push (fifo item)
-  (let* ((tail-tip (fifo-tail-tip fifo))
-         (head (cdr tail-tip))
-         (new-tip (if (null tail-tip)
-                      (let ((tip (cons item nil)))
-                        (setf (cdr tip) tip))
-                      (setf (cdr tail-tip) (cons item head)))))
-    (setf (fifo-tail-tip fifo) new-tip)
-    item))
+  (let* ((head-idx (fifo-head-idx fifo))
+         (tail-idx (fifo-tail-idx fifo)))
+    (cond
+      ((= tail-idx -1) (setf (fifo-head-idx fifo) 0))
+      ((= (next-idx fifo tail-idx) head-idx) (%fifo-extend fifo)))
+    (setf (fifo-tail-idx fifo) (next-idx fifo (fifo-tail-idx fifo))
+          (aref (fifo-queue fifo) (fifo-tail-idx fifo)) item)))
 
 
 (defun fifo-pop (fifo)
-  (when-let ((tail-tip (fifo-tail-tip fifo)))
-    (let ((head (cdr tail-tip)))
-      (prog1 (car head)
-        (if (eq (cdr tail-tip) (cdr head))
-            (setf (fifo-tail-tip fifo) nil)
-            (setf (cdr tail-tip) (cdr head)))))))
+  (let ((head-idx (fifo-head-idx fifo))
+        (tail-idx (fifo-tail-idx fifo)))
+    (unless (= -1 tail-idx)
+      (prog1 (aref (fifo-queue fifo) head-idx)
+        (let ((new-head-idx (next-idx fifo head-idx)))
+          (if (> new-head-idx tail-idx)
+              (setf (fifo-head-idx fifo) -1
+                    (fifo-tail-idx fifo) -1)
+              (setf (fifo-head-idx fifo) (next-idx fifo head-idx))))))))
 
 
 (defun fifo-peek (fifo)
-  (cadr (fifo-tail-tip fifo)))
+  (let ((head-idx (fifo-head-idx fifo)))
+    (unless (= -1 head-idx)
+      (aref (fifo-queue fifo) head-idx))))
 
 
 ;;;
